3def508c3d2621a5d2b6a9272ed16c20bbdba66e
devbotas <simon@devbot.lt>
2021-07-01 13:17:20 +0300

    Updating NuGet.

--- Tinkerforge.AirQualityMonitor/Code/AirQualityProducer.cs
+++ Tinkerforge.AirQualityMonitor/Code/AirQualityProducer.cs
@@ -9,17 +9,17 @@ using Tinkerforge;
 namespace AirQualityMonitor {
     class AirQualityProducer {
         private CancellationTokenSource _globalCancellationTokenSource = new CancellationTokenSource();
-        private ResilientHomieBroker _broker = new ResilientHomieBroker();
+        private IMqttBroker _broker = new PahoBroker();
 
         private HostDevice _device;
-        public HostFloatProperty Pressure;
-        public HostFloatProperty Temperature;
-        public HostFloatProperty Humidity;
-        public HostFloatProperty QualityIndex;
+        public HostNumberProperty Pressure;
+        public HostNumberProperty Temperature;
+        public HostNumberProperty Humidity;
+        public HostNumberProperty QualityIndex;
 
         private DateTime _startTime = DateTime.Now;
-        private HostFloatProperty _systemUptime;
-        private HostStringProperty _systemIpAddress;
+        private HostNumberProperty _systemUptime;
+        private HostTextProperty _systemIpAddress;
 
         public BrickletAirQuality AirQualityBricklet { get; set; }
         public static Logger Log = LogManager.GetCurrentClassLogger();
@@ -34,23 +34,22 @@ namespace AirQualityMonitor {
 
             Log.Info($"Creating Homie properties.");
             _device.UpdateNodeInfo("ambient", "Ambient properties", "no-type");
-            Pressure = _device.CreateHostFloatProperty(PropertyType.State, "ambient", "pressure", "Pressure", 0, "hPa");
-            Temperature = _device.CreateHostFloatProperty(PropertyType.State, "ambient", "temperature", "Temperature", 0, "°C");
-            Humidity = _device.CreateHostFloatProperty(PropertyType.State, "ambient", "humidity", "Humidity", 0, "%");
-            QualityIndex = _device.CreateHostFloatProperty(PropertyType.State, "ambient", "quality-index", "Quality index");
+            Pressure = _device.CreateHostNumberProperty(PropertyType.State, "ambient", "pressure", "Pressure", 0, "hPa");
+            Temperature = _device.CreateHostNumberProperty(PropertyType.State, "ambient", "temperature", "Temperature", 0, "°C");
+            Humidity = _device.CreateHostNumberProperty(PropertyType.State, "ambient", "humidity", "Humidity", 0, "%");
+            QualityIndex = _device.CreateHostNumberProperty(PropertyType.State, "ambient", "quality-index", "Quality index");
 
             _device.UpdateNodeInfo("system", "System", "no-type");
-            _systemUptime = _device.CreateHostFloatProperty(PropertyType.State, "system", "uptime", "Uptime", 0, "h");
-            _systemIpAddress = _device.CreateHostStringProperty(PropertyType.State, "system", "ip-address", "IP address", Program.GetLocalIpAddress());
+            _systemUptime = _device.CreateHostNumberProperty(PropertyType.State, "system", "uptime", "Uptime", 0, "h");
+            _systemIpAddress = _device.CreateHostTextProperty(PropertyType.State, "system", "ip-address", "IP address", Program.GetLocalIpAddress());
 
             Log.Info($"Initializing Homie entities.");
-            _broker.PublishReceived += _device.HandlePublishReceived;
-            _broker.Initialize(mqttBrokerIpAddress, _device.WillTopic, _device.WillPayload, (severity, message) => {
+            _broker.Initialize(mqttBrokerIpAddress);
+            _device.Initialize(_broker, (severity, message) => {
                 if (severity == "Info") { Log.Info(message); }
                 else if (severity == "Error") { Log.Error(message); }
                 else { Log.Debug(message); }
             });
-            _device.Initialize(_broker.PublishToTopic, _broker.SubscribeToTopic);
 
             Task.Run(async () => {
                 Log.Info($"Spinning up parameter monitoring task.");
